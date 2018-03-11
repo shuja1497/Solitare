0fd477e5ce3d31d7d7cfb195b549b15ba3de0966
shuja1497 <shujareshi1497@gmail.com>
2018-03-11 14:20:16 +0530

    made the card clas using java

---
+++ src/App.kt
@@ -0,0 +1,5 @@
+// argument comes first followed by type of argument .. to return : Unit (in place of void )..
+// Unit can be avoided .
+fun main(args: Array<String>) {
+
+}
---
+++ src/Card.java
@@ -0,0 +1,32 @@
+/**
+ * Created by shuja1497 on 3/11/18.
+ */
+public class Card {
+    // Attributes > value(1 ,2 ,3 ..) ; suit : face(up or down)
+
+    private int value ;// immutable . 2 will remain 2 always (so no need of a setter )
+    private String suit ;
+    private boolean faceUp; // mutable.. maybe changed from face up to face down( both getter and setter needed )
+
+    public Card(int value, String suit, boolean faceUp){
+        this.value = value;
+        this.suit = suit;
+        this.faceUp = faceUp;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public boolean isFaceUp() {
+        return faceUp;
+    }
+
+    public void setFaceUp(boolean faceUp) {
+        this.faceUp = faceUp;
+    }
+}
