e71f330d3793b148de58e8af7faebfe722580bbc
shuja1497 <shujareshi1497@gmail.com>
2018-03-11 14:50:35 +0530

    added properties for card class using kotlin

---
+++ src/Card.kt
@@ -0,0 +1,12 @@
+/**
+ * Created by shuja1497 on 3/11/18.
+ */
+// in kotlin we can only declare property .
+// anytime we declare a variable in kotlin its a property
+// val >> immutable
+// var >> mutable
+class Card {
+    val value: Int = 0
+    val suit: String = ""
+    var faceUp: Boolean = false
+}
--- src/JavaCard.java
+++ src/JavaCard.java
@@ -1,14 +1,14 @@
 /**
  * Created by shuja1497 on 3/11/18.
  */
-public class Card {
+public class JavaCard {
     // Attributes > value(1 ,2 ,3 ..) ; suit : face(up or down)
 
     private int value ;// immutable . 2 will remain 2 always (so no need of a setter )
     private String suit ;
     private boolean faceUp; // mutable.. maybe changed from face up to face down( both getter and setter needed )
 
-    public Card(int value, String suit, boolean faceUp){
+    public JavaCard(int value, String suit, boolean faceUp){
         this.value = value;
         this.suit = suit;
         this.faceUp = faceUp;
@@ -30,3 +30,7 @@ public class Card {
         this.faceUp = faceUp;
     }
 }
+
+// fields and properties
+//fields >> value , suit , faceUp
+// properties >> isFaceUp() .... can exist without a field also
