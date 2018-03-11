b67aafafa0ed9fb7d051166f9f9faf0450c0b095
shuja1497 <shujareshi1497@gmail.com>
2018-03-11 20:38:05 +0530

    using the deck

--- src/Deck.kt
+++ src/Deck.kt
@@ -1,8 +1,20 @@
+import java.util.*
+
 /**
  * Created by shuja1497 on 3/11/18.
  */
 class Deck {
     val cards = Array(52, {Card(it % 13, getSuit(it))})
+            // converting card to a list for shuffling and other functions
+
+    var cardsInDeck: MutableList<Card> = cards.toMutableList()
+
+    fun drawCard(): Card = cardsInDeck.removeAt(0)
+
+    fun reset() {
+        cardsInDeck = cards.toMutableList()
+        Collections.shuffle(cardsInDeck)// shuffle the deck
+    }
 
     private fun getSuit(i: Int)= when(i/13){
             0 -> "Clubs"
