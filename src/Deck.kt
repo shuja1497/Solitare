b0e94f318cb97633db15c5e7e56d64055eb140c7
shuja1497 <shujareshi1497@gmail.com>
2018-03-11 20:24:11 +0530

    compacting the deck class

--- src/Deck.kt
+++ src/Deck.kt
@@ -1,26 +1,19 @@
 /**
  * Created by shuja1497 on 3/11/18.
  */
-// will store all 52 cards needed and functions like shuffling and drawing a card .
 class Deck {
-    // using val to make it immutable bcz the 52 card deck will remain the same
-    //val cards: Array<Card> = arrayOf(Card(0,"Clubs"), Card(1, "Clubs"), ...... )
-    // no need of new keyword
-    val fn = fun (i: Int): Card{
-        val value = i%13
-        // when used instead of switch
-        // it can work without an else statement but since we are initializing suit so some value must be provided
-        val suit = when(i/13){
+// lambda expression ....removing function declaration
+    // if there is a single parameter in lambda expression we can omit that
+    // kotlin will automatically initialize that with the name "it"
+    val cards: Array<Card> = Array(52, {Card(it % 13, getSuit(it))})
+
+// single line function can be just written with one line
+    private fun getSuit(i: Int): String = when(i/13){
             0 -> "Clubs"
             1 -> "Diamonds"
             2 -> "Hearts"
-            else -> "Spades"
+            else ->  "Spades"
         }
-        return Card(value, suit)
-    }
-
-    // we can pass in an anonymous func in Array<>
-    // this can be done by removing the function name and assigning the func to a variable
-    // or simply write the whole func as a parameter .
-    val cards: Array<Card> = Array(52, fn)
 }
+
+// data type is not necessary until kotlin can tell the right data type
