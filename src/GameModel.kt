6229cd1ef5a505db1b3aa6f1d4e5c61e36c59c03
shuja1497 <shujareshi1497@gmail.com>
2018-03-11 23:16:44 +0530

    created game model

---
+++ src/GameModel.kt
@@ -0,0 +1,9 @@
+/**
+ * Created by shuja1497 on 3/11/18.
+ */
+// we need to create properties for each of our game objects.
+class GameModel {
+    val deck = Deck()
+    val wastePile:MutableList<Card> = mutableListOf()
+
+}
